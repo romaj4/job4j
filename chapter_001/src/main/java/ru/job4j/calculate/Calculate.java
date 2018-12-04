b803e321fb5e1f1342d76e4003e486258e5089ab
Roman Korolchuk <romat67c@yandex.ru>
2018-12-04 22:56:11 +0300

    1.4 JUnit

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -3,14 +3,24 @@ package ru.job4j.calculate;
 /**
 * Calculate для арифметических операций
 * @author Roman Korolchuk (rom.kor@yandex.ru)
+* @version $Id$
+* @since 0.1
 */
 public class Calculate{
-	
 	/**
-	* main
+	* Main
 	* @param args - args
 	*/
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
 	}
+	
+	/**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Roman Korolchuk (rom.kor@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
