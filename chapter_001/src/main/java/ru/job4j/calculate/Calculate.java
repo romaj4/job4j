360296a0e12be84fbde27b04cf7cd22e31489a59
Roman Korolchuk <romat67c@yandex.ru>
2018-12-04 22:12:57 +0300

    1.3 JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate для арифметических операций
+* @author Roman Korolchuk (rom.kor@yandex.ru)
+*/
 public class Calculate{
 	
+	/**
+	* main
+	* @param args - args
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
 	}
