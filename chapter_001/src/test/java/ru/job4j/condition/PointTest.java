cfd0a40f86140d73edbd5134f51245ad05d02d9a
Roman Korolchuk <romat67c@yandex.ru>
2018-12-08 22:31:00 +0300

    add test for "3.4. Расстояние между точками..."

---
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Roman Korolchuk (rom.kor@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class PointTest {
+
+    @Test
+    public void whenPoint2_2AndPoint5_6ThenDistance5() {
+        Point a = new Point(2, 2);
+        Point b = new Point(5, 6);
+        double dist = a.distanceTo(b);
+        assertThat(dist, closeTo(5, 0.1));
+    }
+}
