d4781a563e8152b38f49fd5989516cf01734a83b
Roman Korolchuk <romat67c@yandex.ru>
2018-12-16 18:05:46 +0300

    Чтение кода и базовые ошибки.

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -9,10 +9,10 @@ package ru.job4j.array;
 public class Square {
 
     public int[] calculate(int bound) {
-        int[] rst = new int[bound];
+        int[] result = new int[bound];
         for (int i = 1; i <= bound; i++) {
-            rst[i - 1] = i * i;
+            result[i - 1] = i * i;
         }
-        return rst;
+        return result;
     }
 }
