27106005f679b2e0bafd50f259c90d11c362452e
Roman Korolchuk <romat67c@yandex.ru>
2018-12-22 19:05:49 +0300

    3. Используя класс StubInput написать тесты проверяющие поведение пользователя.

---
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -0,0 +1,22 @@
+package ru.job4j.tracker;
+
+/**
+ * @author Roman Korolchuk (rom.kor@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class StubInput implements Input {
+
+    private final String[] value;
+
+    private int position;
+
+    public StubInput(final String[] value) {
+        this.value = value;
+    }
+
+    @Override
+    public String ask(String question) {
+        return this.value[this.position++];
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,42 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author Roman Korolchuk (rom.kor@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class StartUITest {
+
+    @Test
+    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+        Tracker tracker = new Tracker();
+        Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findAll()[0].getName(), is("test name"));
+    }
+
+    @Test
+    public void whenUpdateThenTrackerHasUpdatedValue() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Input input = new StubInput(new String[]{"2", item.getId(), "test replace", "заменили заявку", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findById(item.getId()).getName(), is("test replace"));
+    }
+
+    @Test
+    public void whenDeleteItemThenTrackerHasLessValues() {
+        Tracker tracker = new Tracker();
+        Item item1 = tracker.add(new Item("test1", "desc1"));
+        Item item2 = tracker.add(new Item("test2", "desc2"));
+        Input input = new StubInput(new String[]{"3", item1.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findAll().length, is(1));
+        assertThat(tracker.findAll()[0].getName(), is("test2"));
+    }
+}
