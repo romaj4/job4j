e08ddd5c2f6138e8b1409be71956e5036fc14ec1
Roman Korolchuk <romat67c@yandex.ru>
2020-03-30 20:44:18 +0300

    Junit тест для блокирующей очереди.[#152785]

--- chapter_010/src/test/java/ru/job4j/blockingqueue/SimpleBlockingQueueTest.java
+++ chapter_010/src/test/java/ru/job4j/blockingqueue/SimpleBlockingQueueTest.java
@@ -2,10 +2,43 @@ package ru.job4j.blockingqueue;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class SimpleBlockingQueueTest {
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
+        Thread producer = new Thread(
+                () -> IntStream.range(0, 5).forEach(
+                        queue::offer
+                )
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (queue.getSize() != 0 || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
+    }
 
     @Test
     public void whenOneConsumerAndOneProducer() throws InterruptedException {
